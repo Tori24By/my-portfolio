c41e37e888c664d1ab9cb515ed3d4842c793ab05
Vitoria <vitoriadelimaboin@gmail.com>
2026-05-16 01:56:36 -0300

    feat: updated informations

--- src/app/components/skills/skills.ts
+++ src/app/components/skills/skills.ts
@@ -20,16 +20,15 @@ export class Skills {
         { name: 'Angular', category: 'frontend', level: 85 },
         { name: 'TypeScript', category: 'frontend', level: 80 },
         { name: 'HTML / CSS / SCSS', category: 'frontend', level: 90 },
-        { name: 'React', category: 'frontend', level: 70 },
       ],
     },
     {
       name: 'Backend & Banco de Dados',
       skills: [
-        { name: 'Node.js', category: 'backend', level: 75 },
+        { name: 'Java', category: 'backend', level: 55 },
         { name: 'SQL Server', category: 'database', level: 80 },
-        { name: 'PostgreSQL', category: 'database', level: 70 },
-        { name: 'MongoDB', category: 'database', level: 65 },
+        { name: 'PostgreSQL', category: 'database', level: 60 },
+        { name: 'Databricks', category: 'database', level: 65 },
       ],
     },
     {
@@ -38,7 +37,7 @@ export class Skills {
         { name: 'Power Automate', category: 'tools', level: 85 },
         { name: 'SharePoint', category: 'tools', level: 80 },
         { name: 'Git / GitHub', category: 'tools', level: 85 },
-        { name: 'Azure', category: 'tools', level: 60 },
+        { name: 'Azure', category: 'tools', level: 50 },
       ],
     },
   ];
